d7810ca00ee4ecff8367c54f452cfac93d18c609
Rashi Manvar <rashimanvar2005@gmail.com>
2026-04-15 22:12:17 +0530

    Implement palindrome check in 20_valid_Palindrom.java

---
+++ Meduim/20_valid_Palindrom.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+    int j = s.length() - 1;
+    
+    while (i < j) {
+        char c = s.charAt(i);
+        char d = s.charAt(j);
+        
+        if (!Character.isLetterOrDigit(c)) {
+            i++;
+        } else if (!Character.isLetterOrDigit(d)) {
+            j--;
+        } else {
+        
+            if (Character.toLowerCase(c) != Character.toLowerCase(d)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+    }
+    return true;
+    }
+}
