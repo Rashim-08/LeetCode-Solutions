45da21593097447899cdf6d38bd30d07da73fe24
Rashim-08 <rashimanvar2005@gmail.com>
2026-08-14 14:37:23 +0530

    re-did 189

--- Meduim/189_Rotate_Array.java
+++ Meduim/189_Rotate_Array.java
@@ -1,25 +1,25 @@
 class Solution {
+    public void rotate1(int i,int j,int[] nums){
+         while(i<j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+        i++;
+        j--;
+       }
+    }
     public void rotate(int[] nums, int k) {
-        if(k>nums.length)k=k%nums.length;
-        if(k==0)return;
-        int a[]=new int[nums.length];
-        int j=0;
-        for(int i=0;i<nums.length;i++){
-            if(i+k>=nums.length){
-                j=i+k-nums.length;
-            }
-            else{
-                j=i+k;
-            }
-            a[j]=nums[i];
-        }
-       for(int i = 0; i < nums.length; i++){
-    nums[i] = a[i];
-}
-        // for(int x:nums){
-        //     System.out.println(x);
-        // }
-        
-        
+    if (nums.length == 0) return;
+    if(k>=nums.length) {k = k % nums.length;}
+    if (k == 0) return;
+       int i=0;
+       int j=nums.length-1;
+    rotate1(i,j,nums);
+       i=0;
+       j=k-1;
+     rotate1(i,j,nums);
+       i=k;
+       j=nums.length-1;
+    rotate1(i,j,nums);
     }
 }
